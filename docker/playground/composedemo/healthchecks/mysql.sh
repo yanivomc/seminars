5a1dfab9c44823b15370d5da203f1255f6374232
yanivomc-k1 <yanivomc@gmail.com>
2019-10-30 23:49:31 +0200

    Terraform only branch

--- docker/playground/composedemo/healthchecks/mysql.sh
+++
@@ -1,9 +0,0 @@
-
-RESULT=`mysqlshow  music| grep -v Wildcard | grep -o album` 
-if [ "$RESULT" != "album" ]; then
-    echo "MYSQL still down"
-    continue
-else
-    echo "MYSQL IS UP"
-    exit 0
-fi
