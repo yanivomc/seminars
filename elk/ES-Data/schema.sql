cb3001627f32c0f0eb2229261802d1a74fae51ea
yanivomc-k1 <yanivomc@gmail.com>
2020-01-02 23:10:24 +0200

    clean slate

--- elk/ES-Data/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE movielens;
-
-CREATE TABLE movielens.movies (
-	movieID INT PRIMARY KEY NOT NULL,
-	title TEXT,
-	releaseDate DATE
-);
--- terraform/part-2/demos/code-examples/template.tf
+++
@@ -1,7 +0,0 @@
-data "template_file" "web-config" {
-    template = "${file("folderLocation/init.tpl")}"
-
-    vars {
-        dbip = "${aws_instance.database1.private_ip}"
-    }  
-}
