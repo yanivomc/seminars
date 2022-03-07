90c30b336954eca316580907d2b22728bef648da
root <admin@example.com>
2022-03-07 09:26:12 +0200

    updated and working

--- K8S/scripts/generate-kubeconfig.sh
+++ K8S/scripts/generate-kubeconfig.sh
@@ -1,20 +1,18 @@
 #!/usr/bin/env bash
 
 set -e
+sudo snap install jq >&2
+sudo snap install yq >&2
+cat .kube/config | yq -o json > /tmp/config.json
+jsonfile=/tmp/config.json
 
-if [[ $# == 0 ]]; then
-  echo "Usage: $0 SERVICEACCOUNT [kubectl options]" >&2
-  echo "" >&2
-  echo "This script creates a kubeconfig to access the apiserver with the specified serviceaccount and outputs it to stdout." >&2
-
-  exit 1
-fi
+echo "script start" >&2
 
 function _kubectl() {
   kubectl $@ $kubectl_options
 }
 
-serviceaccount="$1"
+serviceaccount="default"
 kubectl_options="${@:2}"
 
 if ! secret="$(_kubectl get serviceaccount "$serviceaccount" -o 'jsonpath={.secrets[0].name}' 2>/dev/null)"; then
@@ -30,23 +28,31 @@ fi
 # context
 context="$(_kubectl config current-context)"
 # cluster
-cluster="development"
+#cluster="development"
+cluster="$(_kubectl config view -o "jsonpath={.contexts[?(@.name==\"$context\")].context.cluster}")"
+
 server="https://$(_kubectl get nodes  --selector '!node-role.kubernetes.io/node' --output jsonpath="{.items[?(@.status.addresses[-1].type)].status.addresses[1].address}")"
 # token
 ca_crt_data="$(_kubectl get secret "$secret" -o "jsonpath={.data.ca\.crt}" | openssl enc -d -base64 -A)"
 namespace="$(_kubectl get secret "$secret" -o "jsonpath={.data.namespace}" | openssl enc -d -base64 -A)"
 token="$(_kubectl get secret "$secret" -o "jsonpath={.data.token}" | openssl enc -d -base64 -A)"
-
+clientkeydata="$(mktemp)"; cat $jsonfile | jq ".users[0].user.\"client-key-data\""  | tr -d '"' | base64 --decode > $clientkeydata
+clientcertificate="$(mktemp)"; cat $jsonfile | jq ".users[0].user.\"client-certificate-data\"" | tr -d '"' | base64 --decode > $clientcertificate
 
 export KUBECONFIG="$(mktemp)"
 
-kubectl config set-credentials "$serviceaccount" --token="$token" >/dev/null
+#kubectl config set-credentials developer --client-certificate="$ca_crt" --token="$token" >/dev/null
 ca_crt="$(mktemp)"; echo "$ca_crt_data" > $ca_crt
 #kubectl config --kubeconfig=kube-jb set-cluster "$cluster" --certificate-authority="$ca_crt" --embed-certs >/dev/null
 
-kubectl config set-cluster "$cluster" --server="$server" --certificate-authority="$ca_crt" --embed-certs >/dev/null
-kubectl config set-context "$cluster" --cluster="$cluster" --namespace="$namespace" --user="$serviceaccount" >/dev/null
-kubectl config use-context "$cluster" >/dev/null
+kubectl config set-cluster development --server="$server" --insecure-skip-tls-verify  >/dev/null
+kubectl config set-credentials "developer" --client-certificate="$clientcertificate" --client-key="$clientkeydata" --embed-certs=true >/dev/null
 
+kubectl config set-context dev-jb --cluster=development --namespace=default --user=developer >/dev/null
+kubectl config use-context dev-jb >/dev/null
+printf "\n\n ******  COPY FROM HERE EXCLUDE THIS LINE ******\n\n" >&2
 cat "$KUBECONFIG"
+printf "\n\n******  END COPY EXCLUDE THIS LINE ******\n\n" >&2
+echo "Create a ~/.kube/config-jb in your local machine and paste the above into it"
+echo "Once pasted run: export KUBECONFIG=~/.kube/config-jb && kubectl config use-context dev-jb"
 # vim: ft=sh :
